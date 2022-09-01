a75e184baa205ed5948dfe4a252902bffad72af7
Caroline Souza Camargo <carolinecamargo012@gmail.com>
2022-09-01 09:48:11 -0300

    colocando código em funções

--- exer_5.c
+++ exer_5.c
@@ -9,131 +9,80 @@
 
 //Bibliotecas
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-
-//Variáveis globais
-long int **mat_1, contador[6][6];
-
 //Protótipos Funções
-void lendoarquivo(int maxAltura, int maxLargura, int b, int c, int d);
+void LendoArquivo(int **matriz, int maxAltura, int maxLargura, int a, int b, int c, int d, int quadroPesquisar);
+int VerificandoProfundidade(int colunaPesquisar, int linhaPesquisar, int quadroPesquisar);
 
-//Função Main
+//Função Main Para Testes
 int main()
 {
-    FILE *arquivoLeitura, *arquivoescrita;
-    int coluna_1, linha_1;
-    char nomearquivo_1[40], nome_csv[100]="55-43 Video touchdown_pass_1080p_60f(numeros).csv";
-    coluna_1 = 1920/4;
-    linha_1 = 1080/4;
-    
-    strcpy(nomearquivo_1, "csvs - tamanho de bloco/touchdown_pass_1080p_60f/av1_55.csv");    //cq maior
-    
-    //Zera contador
-    for (int i=0; i<6; i++) {
-        for (int j=0; j<6; j++) {
-            contador[i][j]=0;
-        }
-    }
-
-    // Aloca um vetor de LIN ponteiros para linhas
-    mat_1 = malloc(linha_1 * sizeof (int*));
-
-    // Aloca cada uma das linhas (vetores de COL inteiros)
-    for (int i=0; i<linha_1; i++) {
-        mat_1[i] = malloc(coluna_1 * sizeof (int*));
-    }
-    
-    // Percorre a matriz e preenche com noves
-    for (int i=0; i<linha_1; i++) {
-        for (int j=0; j<coluna_1; j++) {
-            mat_1[i][j]=9;              //Coloquei nove para saber quando não é um número do csv          
-        }
-    }
-
-    // Abrindo arquivo CSV
-    arquivoLeitura= fopen(nomearquivo_1, "r");
-    if (arquivoLeitura == NULL) {
-        printf("\nErro ao abrir o arquivo.\n");
-        return 1;
-    }
-    
-    // Percorrendo linhas do arquivo CSV 
-    char cabecalho_1[100];
-    int a_1, b_1, c_1, d_1, e_1;
-    int index=0;
-    fscanf(arquivoLeitura, "%s", cabecalho_1);
-
-    // a:num_quadro     b:pos_coluna     c:pos_linha      d:tam_bloco     e:lixo
-    while (fscanf(arquivoLeitura, "%d,%d,%d,%d,%d", &a_1, &b_1, &c_1, &d_1, &e_1) != EOF) {
-        lendoarquivo(linha_1, coluna_1, b_1, c_1, d_1);
-        index=a_1;
-    }
-    
-    //Calculando Percentual
-    double contador_percentual[6][6];
-    for (int i=0; i<6; i++) {
-        for (int j=0; j<6; j++) {
-            double aa, bb, cc, dd, ee, ff;
-            aa=contador[i][0];
-            bb=contador[i][1];
-            cc=contador[i][2];
-            dd=contador[i][3];
-            ee=contador[i][4];
-            ff=contador[i][5];
-            contador_percentual[i][j] = 100 * (contador[i][j] / (aa + bb + cc + dd + ee + ff));
-            if ((aa + bb + cc + dd + ee + ff) == 0){
-                contador_percentual[i][j]=0;
-            }
-        }
-    }
-
-    arquivoescrita = fopen(nome_csv, "w");
-    if (arquivoescrita == NULL) {
-        printf("\nErro ao abrir o arquivo.\n");
-        return 1;
-    }
-
-    //Mostrando resultados
-    printf("\n\nRelacao Numerica de profundidade entre o CQ maior e o CQ menor\n");
-    printf("\n\t0\t1\t2\t3\t4\t5\n\n");
-    for (int i=0; i<6; i++) {
-        printf("%d\t",i);
-        for (int j=0; j<6; j++) {
-            printf("%d\t",contador[i][j]);
-            fprintf(arquivoescrita, "%d,", contador[i][j]);
-        }
-        printf("\n");
-        fprintf(arquivoescrita, "\n");
-    }
- 
-
-    printf("\nRelacao percentual\n");
-    printf("\n\t0\t\t1\t\t2\t\t3\t\t4\t\t5\n\n");
-    for (int i=0; i<6; i++) {
-        printf("%d\t",i);
-        for (int j=0; j<6; j++) {
-            printf("%f\t",contador_percentual[i][j]);
-            //fprintf(arquivoescrita, "%f,", contador_percentual[i][j]);
-        }
-        printf("\n");
-        //fprintf(arquivoescrita, "\n");
-    }
-
-    fclose(arquivoescrita);
-    fclose(arquivoLeitura);
-
-    // libera a memória da matriz
-    for (int i=0; i<linha_1; i++) {
-        free(mat_1[i]);
-    }
-    free(mat_1);
-
+    int colunaPesquisar = 72, linhaPesquisar = 0, quadroPesquisar = 0;
+	int profundidadeCQMaior = VerificandoProfundidade(colunaPesquisar, linhaPesquisar, quadroPesquisar);
+    printf("Profundidade: %d", profundidadeCQMaior);
     printf("\nFim do programa\n");
 }
 
-void lendoarquivo(int maxAltura, int maxLargura, int b, int c, int d)
+/* NO LIBAOM:   mi_col: coluna
+                mi_row: linha
+                cm->current_frame.order_hint: quadro codificado */      
+int VerificandoProfundidade(int colunaPesquisar, int linhaPesquisar, int quadroPesquisar) {
+	FILE *arquivoLeitura;
+	char nomeArquivo[] = "teste.csv";    							//Nome do arquivo gerado pelo CQ maior
+    int colunaMatriz = 1920/4, linhaMatriz = 1080/4;				//Resolução do vídeo 1920x1080 (FullHD)
+	int **matriz;
+
+    // Aloca um vetor de ponteiros para as linhas e depois aloca espaço para as colunas 
+    matriz = malloc(linhaMatriz * sizeof (int*));
+    for (int i = 0; i < linhaMatriz; i++) {
+        matriz[i] = malloc(colunaMatriz * sizeof (int*));
+    }
+    
+    // Percorre a matriz de ponteiros e preenche com noves
+    for (int i = 0; i < linhaMatriz; i++) {
+        for (int j = 0; j < colunaMatriz; j++) {
+            matriz[i][j] = 9;              						    //Coloquei nove para saber quando não é um número do csv          
+        }
+    }
+
+    // Abrindo arquivo CSV para leitura
+    arquivoLeitura = fopen(nomeArquivo, "r");
+    if (arquivoLeitura == NULL) {
+        printf("\nErro ao abrir o arquivo.\n");
+    }
+    
+    // Percorrendo linhas do arquivo CSV 
+    char cabecalho[45];
+    int a, b, c, d, e, profundidade;
+    fscanf(arquivoLeitura, "%s", cabecalho);
+
+    // a:num_quadro     b:pos_coluna     c:pos_linha      d:tam_bloco     e:lixo
+    while (fscanf(arquivoLeitura, "%d,%d,%d,%d,%d", &a, &b, &c, &d, &e) != EOF) {
+        LendoArquivo(matriz, linhaMatriz, colunaMatriz, a, b, c, d, quadroPesquisar);
+    }
+
+	// Verificando profundidade do CQ Maior na regiao especificada
+	for (int i = 0; i < linhaMatriz; i++) {
+        for (int j = 0; j < colunaMatriz; j++) {
+			if (i == linhaPesquisar && j == colunaPesquisar) {
+				profundidade = matriz[i][j];
+			}
+        }
+    }
+    
+    // fecha arquivo e libera a memória da matriz
+    fclose(arquivoLeitura);
+    for (int i = 0; i < linhaMatriz; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
+
+    //retorna a profundidade encontrada (essa função vai estar no inicio do codigo)
+    return profundidade;
+}
+
+void LendoArquivo(int **matriz, int maxAltura, int maxLargura, int a, int b, int c, int d, int quadroPesquisar)
 {
     /*  (a) número do quadro 
 		(b) posição da coluna (largura) 
@@ -236,11 +185,70 @@ void lendoarquivo(int maxAltura, int maxLargura, int b, int c, int d)
     
     //(b) posição da coluna (largura);
     //(c) posição da linha (altura);
-    for (int i=c; i < (c + altura); i++) {
-        for (int j=b; j < (b + largura); j++) {
-            if (i < maxAltura && j < maxLargura) {
-                mat_1[i][j] = profundidade;
-            }
+    for (int i = c; i < (c + altura); i++) {
+        for (int j = b; j < (b + largura); j++) {
+			if (quadroPesquisar == a) {
+				if (i < maxAltura && j < maxLargura) {
+                	matriz[i][j] = profundidade;
+            	}
+			}
         }
     }
 }
+
+void AplicandoProfundidade(int profundidadeAtual, int profundidadeCQMaior) {
+    // Verificando técnica que será implementada
+	if (profundidadeCQMaior >=0 && profundidadeCQMaior <= 3) {          //REGRA 1: Mesmo nível de profundidade ou um nível a mais
+		if (profundidadeAtual == profundidadeCQMaior || profundidadeAtual == (profundidadeCQMaior + 1)) {
+            /* particiona */
+            /*
+            part_search_state.do_square_split              = true;
+            part_search_state.partition_none_allowed       = false;
+            part_search_state.do_rectangular_split         = false;
+            part_search_state.partition_rect_allowed[HORZ] = false;
+            part_search_state.partition_rect_allowed[VERT] = false;
+            part_search_state.found_best_partition         = false;
+            part_search_state.terminate_partition_search   = false;
+            */
+        } else {
+            /* não particiona */
+            /* 
+            part_search_state.do_square_split               = false;
+            part_search_state.partition_none_allowed        = true;
+            part_search_state.do_rectangular_split         &= true;
+            part_search_state.partition_rect_allowed[HORZ] &= true;
+            part_search_state.partition_rect_allowed[VERT] &= true;
+            part_search_state.found_best_partition          = true;
+            part_search_state.terminate_partition_search    = true; 
+            */
+        }
+	} else if (profundidadeCQMaior > 3 && profundidadeCQMaior <=5 ) {  //REGRA 2: Evita-se níveis de profundidade menores que 3
+		if (profundidadeAtual >= 3) {
+            /* particiona */
+            /*
+            part_search_state.do_square_split              = true;
+            part_search_state.partition_none_allowed       = false;
+            part_search_state.do_rectangular_split         = false;
+            part_search_state.partition_rect_allowed[HORZ] = false;
+            part_search_state.partition_rect_allowed[VERT] = false;
+            part_search_state.found_best_partition         = false;
+            part_search_state.terminate_partition_search   = false;
+            */
+        } else {
+            /* não particiona */
+            /* 
+            part_search_state.do_square_split               = false;
+            part_search_state.partition_none_allowed        = true;
+            part_search_state.do_rectangular_split         &= true;
+            part_search_state.partition_rect_allowed[HORZ] &= true;
+            part_search_state.partition_rect_allowed[VERT] &= true;
+            part_search_state.found_best_partition          = true;
+            part_search_state.terminate_partition_search    = true; 
+            */
+        }
+	} else {
+        printf("\nERRO! (Essa profundidade nao deveria existir)");
+    }
+}
+
+						
